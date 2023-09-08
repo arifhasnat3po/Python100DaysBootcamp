ce5c2e809a0153ee5e697b115a0700856862a77a
Arif <ahparif.16@gmail.com>
2023-09-09 01:54:23 +0600

    Beginner

---
+++ Day -2/Datatypes.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# print(two_digit_number,[0])
+# type(two_digit_number)
+# print(type(two_digit_number))
+
+# print(len(two_digit_number))
+
+# numbers = int(two_digit_number)
+sum = int(two_digit_number[0] )+ int(two_digit_number[1])
+# first_number = numbers[0]
+# print("first_number")
+# print(sum)
+print("Sum of the two digit number:",sum)
