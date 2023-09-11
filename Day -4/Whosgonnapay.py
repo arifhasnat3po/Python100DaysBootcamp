cbe671b2d3df71d1e54a22367564ac4fa3c3cba6
Arif <ahparif.16@gmail.com>
2023-09-12 03:27:45 +0600

    Beginner

--- Day -4/Whosgonnapay.py
+++ Day -4/Whosgonnapay.py
@@ -9,9 +9,10 @@ names = names_string.split(", ")
 # print(names)
 # name_list = names
 # print(len(names))
-lenth_of_list = len(names)
-# print(lenth_of_list)
-random_number = random.randint(0, lenth_of_list -1)
-pay_by = names[random_number]
-# print(random_number)
+# lenth_of_list = len(names)
+# # print(lenth_of_list)
+# random_number = random.randint(0, lenth_of_list -1)
+# pay_by = names[random_number]
+# # print(random_number)
+pay_by = random.choice(names)
 print(f"{pay_by} is going to buy the meal today!")
