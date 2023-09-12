30fcd6ef27b26843e7ef33a571e8e8c2630a02e3
Arif <ahparif.16@gmail.com>
2023-09-13 02:55:12 +0600

    Beginner

--- Day -4/RockPaperScissors.py
+++ Day -4/RockPaperScissors.py
@@ -25,11 +25,13 @@ scissors = '''
        __________)
       (____)
 ---.__(___)
-'''
-game_images = [rock, paper, scissors]
+# '''
+# game_images = [rock, paper, scissors]
 user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 if user_input >= 3 or user_input < 0:
  print("Please enter a valid number")
+else:
+    game_images = [rock, paper, scissors]
 # if user_input == 0:
 #     print(rock)
 # elif user_input == 1:
