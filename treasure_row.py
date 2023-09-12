a4904d4f90e2b6c2c383eb4ad76f9a5a5d0542ed
Arif <ahparif.16@gmail.com>
2023-09-13 01:14:18 +0600

    Beginner

---
+++ treasure_row.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+map_list_horizontal = int(position[0])
+map_list_vertical = int(position[1])
+
+# map [map_list_horizontal - 1][map_list_vertical - 1] = "X"
+# print([map_list_vertical], [map_list_horizontal])
+map [map_list_vertical -1 ][map_list_horizontal -1] = "X"
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
