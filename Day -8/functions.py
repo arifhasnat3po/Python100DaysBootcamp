899f2dd9c49863bd98ab38fc8e51eedb349f6629
Arif <ahparif.16@gmail.com>
2023-09-20 02:28:16 +0600

    Beginner

---
+++ Day -8/functions.py
@@ -0,0 +1,19 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# def greet():
+#   print("Hello")
+#   print("Assalamu Alaikum")
+#   print("Namaste")
+
+# greet()
+def greet_with(name, location):
+  print(f"Your name is {name}.")
+  print(f"Your location is {location}")
+
+greet_with(location="Nowhere",name = "NoOne")
+
+
+
+
