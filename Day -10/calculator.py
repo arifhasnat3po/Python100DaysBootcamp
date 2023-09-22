de2126d908f4a6b7af1ee03c0609fe5b2f2a94e6
Arif <ahparif.16@gmail.com>
2023-09-23 02:50:05 +0600

    Beginner

--- Day -10/calculator.py
+++ Day -10/calculator.py
@@ -25,19 +25,42 @@ operations = {
     "*": mul,
     "/": div,
 }
-
+def calculator():
 # operations["*"]
-num1 = int(input("What's the first number?: "))
-# num2 = int(input("What's the second number?: "))
+  num1 = int(input("What's the first number?: "))
+  # num2 = int(input("What's the second number?: "))
+  
+  for i in operations:
+    print(i)
+  
+  
+  operation_symbol = input("Pick an operation: ")
+  num2 = int(input("What's the next number?: "))
+  
+  calculation = operations[operation_symbol]
+  answer = calculation(num1,num2)
+  
+  # print(test)
+  print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+  
+  should_continue = True
+  
+  while should_continue:
+    
+  
+    operation_symbol = input("Pick another operation: ")
+    num3 = int(input("What's the next number?: "))
+    calculation = operations[operation_symbol]
+    second_answer = calculation(calculation(num1, num2), num3)
+    print(f"{num1} {operation_symbol} {num2} = {second_answer}")
+  
+    if input(f"Type 'y' to continue with {answer}, or type 'n' to exit for new calculation :").lower() =='y':
+      num1 = answer
+  
+    else:
+      should_continue = False
+      calculator()
 
-for i in operations:
-  print(i)
-
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-
-calculation = operations[operation_symbol]
-answer = calculation(num1,num2)
-
-# print(test)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
+  
