3090f643281444ab134aa0b203a7d69ed46f865a
Arif <ahparif.16@gmail.com>
2023-10-09 03:11:40 +0600

    Intermediate

---
+++ Day -18/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+tim.color("spring green	")
+
+# for _ in range(7):
+#     tim.forward(100)
+#     tim.right(72)
---
+++ Day -18/turtlethree.py
@@ -0,0 +1,63 @@
+from turtle import Turtle, Screen
+tim = Turtle()
+tim.shape("turtle")
+
+
+tim.color("deep sky blue")
+for _ in range(4):
+    tim.forward(100)
+    tim.right(120)
+    
+tim.left(0)
+tim.color("dark slate blue")
+
+for _ in range(5):
+    tim.forward(100)
+    tim.right(90)
+    
+tim.color("spring green")
+
+for _ in range(5):
+    tim.forward(100)
+    tim.right(72)   
+ 
+ 
+    
+tim.color("sea green")
+
+for _ in range(6):
+    tim.forward(100)
+    tim.right(60)
+    
+    
+    
+tim.color("blue")
+
+for _ in range(8):
+    tim.forward(100)
+    tim.right(51)
+    
+    
+tim.color("dark slate blue")
+for _ in range(9):
+    tim.forward(100)
+    tim.right(45)
+    
+tim.color("aquamarine")
+for _ in range(10):
+    tim.forward(100)
+    tim.right(40)
+
+    
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
