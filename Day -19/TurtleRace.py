734590d18548457528d1861d5779584030c79cd0
Arif <ahparif.16@gmail.com>
2023-10-13 02:47:12 +0600

    Intermediate

--- Day -19/TurtleRace.py
+++ Day -19/TurtleRace.py
@@ -1,14 +1,15 @@
 from turtle import Turtle, Screen
 
-
-tim = Turtle()
 screen = Screen()
 
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title = "Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "green","yellow", "blue", "purple"]
 print(user_bet)
 
-tim = Turtle()
+tim = Turtle(shape = "turtle")
+tim.penup()
+
 tim.goto(x=-230, y=-100)
 
 
