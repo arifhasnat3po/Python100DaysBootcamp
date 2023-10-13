57972108c8a50beebe8c4843fba0ea72ab90e7dd
Arif <ahparif.16@gmail.com>
2023-10-14 01:27:00 +0600

    Intermediate

--- Day -19/TurtleRace.py
+++ Day -19/TurtleRace.py
@@ -5,13 +5,51 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title = "Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "green","yellow", "blue", "purple"]
-print(user_bet)
 
-tim = Turtle(shape = "turtle")
-tim.penup()
+y_positions = [-100,-60,-20, 20, 60, 100]
 
-tim.goto(x=-230, y=-100)
+for i in range(0, 6):   
 
+    tim = Turtle(shape = "turtle")
+    tim.color(colors[0])
+    tim.penup()
+
+    tim.goto(x=-230, y=y_positions[i])
+
+
+tom = Turtle(shape = "turtle")
+tom.color(colors[1])
+tom.penup()
+
+tom.goto(x=-230, y=-60)
+
+
+
+dom = Turtle(shape = "turtle")
+dom.color(colors[2])
+dom.penup()
+
+dom.goto(x=-230, y=-20)
+
+
+com = Turtle(shape = "turtle")
+com.color(colors[3])
+com.penup()
+
+com.goto(x=-230, y=20)
+
+
+bom = Turtle(shape = "turtle")
+bom.color(colors[4])
+bom.penup()
+
+bom.goto(x=-230, y=60)
+
+aom = Turtle(shape = "turtle")
+aom.color(colors[5])
+aom.penup()
+
+aom.goto(x=-230, y=100)
 
 
 screen.exitonclick()
