94366cd0b442654b302e28a7ebf10771a9a20122
Arif <ahparif.16@gmail.com>
2023-10-21 02:48:29 +0600

    Snake Game

--- Day -20/snake_game.py
+++ Day -20/snake_game.py
@@ -43,14 +43,18 @@ while game_is_on:
     #Detection Collision with wall
     
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
-        game_is_on = False
-        score.game_over()
+        # game_is_on = False
+        # score.game_over()
+        score.reset()
+        snake.reset()
 
     #Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            # game_is_on = False
+            # score.game_over()
+            score.reset()
+            snake.reset()
 
 
 
