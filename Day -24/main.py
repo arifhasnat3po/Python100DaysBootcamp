55192edb379b1c28e133f40b9a1107d5a6e50267
Arif <ahparif.16@gmail.com>
2023-10-22 02:48:57 +0600

    Intermediate

---
+++ Day -24/main.py
@@ -0,0 +1,9 @@
+# file = open("my_file.txt")
+# with  open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#     file.close()
+
+
+with  open("my_file.txt", mode="a") as file:
+    file.write("\nNew text.")
