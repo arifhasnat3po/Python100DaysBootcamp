62818f1e6292b1413917616960237caaa371afe9
Arif <ahparif.16@gmail.com>
2023-11-03 04:37:16 +0600

    TKinter

--- Day -27/TKinter.py
+++ Day -27/TKinter.py
@@ -5,6 +5,12 @@ window.title("My first GUE program")
 window.minsize(width=500, height=300)
 
 
+#Label
+my_label = tkinter.Label(text="You are a Human", font=("Arial", 24, "bold"))
+
+
+my_label.pack()
+
 
 
 
