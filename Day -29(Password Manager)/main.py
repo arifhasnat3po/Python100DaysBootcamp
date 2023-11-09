9ca4fc86c1552fe89f97bc08c10b9e5b74696788
Arif <ahparif.16@gmail.com>
2023-11-10 03:35:02 +0600

    Password Generator

--- Day -29(Password Manager)/main.py
+++ Day -29(Password Manager)/main.py
@@ -3,8 +3,20 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
 
+def save():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    if len(password) > 0:
+        with open('pass.txt', 'a') as f:  # Use 'a' for append mode
+            f.write(f"{website} | {email} | {password}\n")
+            # Clear the entry widgets
+            website_input.delete(0, 'end')
+            password_input.delete(0, 'end')
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
 
@@ -19,7 +31,8 @@ website_label = Label(text="Website:")
 website_label.grid(column=0, row=3)
 
 website_input = Entry(width=35)
-website_input.get()
+website_input.focus()
+# website_input.get()
 website_input.grid(column=1, columnspan=2, row=3)
 
 
@@ -27,22 +40,23 @@ email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=4)
 
 email_input = Entry(width=35)
-email_input.get()
-email_input.grid(column=1, columnspan=2, row=4)
+# email_input.get()
 
+email_input.grid(column=1, columnspan=2, row=4)
+email_input.insert(0, "example@gamil.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=5)
 
 password_input = Entry(width=35)
-password_input.get()
+# password_input.get()
 password_input.grid(column=1, columnspan=2, row=5)
 
 
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=5)
 
-add_button = Button(text="Add", width=30)
+add_button = Button(text="Add", width=30, command=save)
 add_button.grid(column=1, columnspan=2, row=6)
 
 
