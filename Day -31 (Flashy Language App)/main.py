554ab0e0f4183d710f76c145ab26ec7b88448b8a
Arif <ahparif.16@gmail.com>
2023-11-20 05:03:21 +0600

    Flashy

--- Day -31 (Flashy Language App)/main.py
+++ Day -31 (Flashy Language App)/main.py
@@ -4,22 +4,25 @@ import random
 import time
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pd.read_csv(
-    r"C:\Users\ahpar\Documents\100daysofPython\Day -31 (Flashy Language App)\data\german_words.csv")
-print(data)
-df = data.to_dict(orient="records")
-# print(df)
-# word = df[random.randint(0, len(df)-1)]["German"]
-# print(word)
-
 current_card = {}
+to_learn = {}
+
+try:
+    data = pd.read_csv(
+        r"C:\Users\ahpar\Documents\100daysofPython\Day -31 (Flashy Language App)\data\words_to_learn.csv")
+
+except FileNotFoundError:
+    original_data = pd.read_csv(
+        r"C:\Users\ahpar\Documents\100daysofPython\Day -31 (Flashy Language App)\data\german_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(df)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="German", fill="black")
     canvas.itemconfig(card_word, text=current_card["German"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
@@ -32,6 +35,14 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
     # flip_timer = window.after(3000, func=next_card)
+
+
+def is_known():
+
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -57,9 +68,7 @@ unknown_button.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-# check_image.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-known_button = Button(image=check_image, command=next_card)
-# canvas.create_image(790, 300, image=check_image)
+known_button = Button(image=check_image, command=is_known)
 known_button.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 known_button.grid(row=1, column=1)
 
