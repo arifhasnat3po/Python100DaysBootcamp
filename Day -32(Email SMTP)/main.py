977a6b9b9421870fff7285de76df5557074ecbd4
Arif <ahparif.16@gmail.com>
2023-11-22 04:31:00 +0600

    Email SMTP

--- Day -32(Email SMTP)/main.py
+++ Day -32(Email SMTP)/main.py
@@ -1,8 +1,24 @@
-import smtplib
+# import smtplib
 
-my_mail = ""
-password = ""
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_mail, password)
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+# connection.login(user=my_mail, password=password)
+# connection.sendmail(from_addr=my_mail,
+#                     to_addrs="ccesaintmartintour2023@gmail.com", msg="Subject: Test mail\n\nHello World!")
+
+# connection.close()
+import datetime as dt
+
+now = dt.datetime.now()
+hour = now.hour
+year = now.year
+month = now.month
+day = now.day
+
+print(hour)
+print(day)
+print(now)
+
+date_of_birth = dt.datetime(year=1999, month=5, day=2)
+print(date_of_birth)
