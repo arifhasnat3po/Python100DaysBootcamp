4ecc4de891a38a60bfdb3a02fd50da8fbffaf82c
Arif <ahparif.16@gmail.com>
2023-11-28 03:45:40 +0600

    API Quiz

--- Day -34(Quiz App)/002 quizzler-app-start/ui.py
+++ Day -34(Quiz App)/002 quizzler-app-start/ui.py
@@ -1,2 +1,12 @@
+from tkinter import *
+import tkinter
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.canvas = Canvas(width=200, height=224, bg=THEME_COLOR)
+        self.window.mainloop()
