72f7119bfb49f2f8bdaeebcb39d1b46156c76f83
Arif <ahparif.16@gmail.com>
2023-12-01 03:19:02 +0600

    API Quiz

--- Day -34(Quiz App)/002 quizzler-app-start/ui.py
+++ Day -34(Quiz App)/002 quizzler-app-start/ui.py
@@ -42,9 +42,16 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.score_label.config(text=f"Score: {self.quiz.score}")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            # self.canvas.config(bg="white")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(
+                self.question_text, text="You've actually reach the end of the Quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_pressed(self):
         # is_right =  self.quiz.check_answer("True")
