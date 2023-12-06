e9ef7cc8cfce11e76038cb276ce9651e44bf5f94
Arif <ahparif.16@gmail.com>
2023-12-07 04:58:34 +0600

    POST

--- Day -37(Habit Tracker)/main.py
+++ Day -37(Habit Tracker)/main.py
@@ -1,11 +1,14 @@
 import requests
 
+USERNAME = "arifhasnat"
+TOKEN = "jhfldkajshlfkjhaslkajdshf"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 
 user_params = {
-    "token": "jhfldkajshlfkjhaslkajdshf",
-    "username": "arifhasnat",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 
@@ -13,3 +16,21 @@ user_params = {
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Reading Books",
+    "unit": "page",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(
+    url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
