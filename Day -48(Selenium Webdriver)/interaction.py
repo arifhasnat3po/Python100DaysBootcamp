6faaa3bc806571d5a4e536e14f4dbbf322e10a6b
Arif <ahparif.16@gmail.com>
2024-01-03 03:26:20 +0600

    Selenium

--- Day -48(Selenium Webdriver)/interaction.py
+++ Day -48(Selenium Webdriver)/interaction.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 ID = "id"
 NAME = "name"
 XPATH = "xpath"
@@ -20,12 +21,24 @@ service = ChromeService(chrome_driver_path)
 
 driver = webdriver.Chrome(service=service, options=option)
 
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
 
 # Find the element representing the article count
-article_number_element = driver.find_element(CSS_SELECTOR, "articlecount a")
-article_number_element.click()
+# article_number_element = driver.find_element(CSS_SELECTOR, "articlecount a  ")
+# article_number_element.click()
+# encyclopedia = driver.find_element(LINK_TEXT, "encyclopedia")
+# encyclopedia.click()
+# search_text = driver.find_element(NAME, "search")
+# search_text.send_keys("Python")
+# search_text.submit()
+# Find form elements by name and fill them out
+driver.find_element(By.NAME, "fName").send_keys("Arif")
+driver.find_element(By.NAME, "lName").send_keys("Hasnat")
+driver.find_element(By.NAME, "email").send_keys("xyz@example.com")
 
+# Submit the form
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
 # # Get the text content of the element
 # article_number = article_number_element.text
