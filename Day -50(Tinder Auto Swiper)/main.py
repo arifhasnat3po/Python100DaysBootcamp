97a4a615e6cf2fd8c23c77b7e76bbf11fff8845f
Arif <ahparif.16@gmail.com>
2024-01-05 05:18:52 +0600

    Automatic Tinder Swiper

---
+++ Day -50(Tinder Auto Swiper)/main.py
@@ -0,0 +1,30 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+ACCOUNT_PASSWORD = ""
+PHONE = ""
+
+
+ID = "id"
+NAME = "name"
+XPATH = "xpath"
+LINK_TEXT = "link text"
+PARTIAL_LINK_TEXT = "partial link text"
+TAG_NAME = "tag name"
+CLASS_NAME = "class name"
+CSS_SELECTOR = "css selector"
+
+chrome_options = Options()
+option = chrome_options.add_argument("--headless")
+
+chrome_driver_path = "C:\\Development\\chromedriver-win64\\chromedriver.exe"
+service = ChromeService(chrome_driver_path)
+
+driver = webdriver.Chrome(service=service, options=option)
+driver.get("")
+
+sign_in_button = driver.find_element(LINK_TEXT, "Sign in")
+sign_in_button.click()
