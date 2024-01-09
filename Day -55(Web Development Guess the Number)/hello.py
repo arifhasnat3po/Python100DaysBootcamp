e107b3fb0f8a7707a79bf0dcfd59cf67a88b7e32
Arif <ahparif.16@gmail.com>
2024-01-10 04:01:27 +0600

    FLASK

---
+++ Day -55(Web Development Guess the Number)/hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/username/<name>')
+def greeting(name):
+    return f"Hello there {name}"
+
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
