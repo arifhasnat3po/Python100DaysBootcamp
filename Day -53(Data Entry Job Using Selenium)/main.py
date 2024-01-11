158d81e4722cabfc891489ba69101fdab9df2467
Arif <ahparif.16@gmail.com>
2024-01-11 20:53:05 +0600

    FLASK

--- Day -53(Data Entry Job Using Selenium)/main.py
+++ Day -53(Data Entry Job Using Selenium)/main.py
@@ -4,21 +4,43 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
 from bs4 import BeautifulSoup
 import requests
 
-# response = requests.get("https://news.ycombinator.com/")
+response = requests.get("https://www.zillow.com/san-francisco-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A%7B%22north%22%3A37.897307075079304%2C%22south%22%3A37.65307512758795%2C%22east%22%3A-122.25136844042969%2C%22west%22%3A-122.61529055957031%7D%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3Anull%2C%22max%22%3A872627%7D%2C%22mp%22%3A%7B%22min%22%3Anull%2C%22max%22%3A3000%7D%2C%22beds%22%3A%7B%22min%22%3A1%2C%22max%22%3Anull%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22mapZoom%22%3A11%7D")
 
 yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
-soup.title
-print(soup.title)
-# from selenium.webdriver.common.keys import Keys
-# INSTAGRAM_ACCOUNT_PASSWORD = ""
-# EMAIL = ""
-# SIMILAR_ACCOUNT = ""
+
+# all_link_elements = soup.select(".list-card-top a")
+
+all_links = []
+# for link in all_link_elements:
+#     href = link["href"]
+#     print(href)
+#     if "http" not in href:
+#         all_links.append(f"https://www.zillow.com{href}")
+#     else:
+#         all_links.append(href)
+
+# all_address_elements = soup.select(".list-card-info address")
+# all_addresses = [address.get_text().split(" | ")[-1]
+#                  for address in all_address_elements]
+
+# all_price_elements = soup.select(".list-card-heading")
+# all_prices = []
+# for element in all_price_elements:
+#     # Get the prices. Single and multiple listings have different tag & class structures
+#     try:
+#         # Price with only one listing
+#         price = element.select(".list-card-price")[0].contents[0]
+#     except IndexError:
+#         print('Multiple listings for the card')
+#         # Price with multiple listings
+#         price = element.select(".list-card-details li")[0].contents[0]
+#     finally:
+#         all_prices.append(price)
 ID = "id"
 NAME = "name"
 XPATH = "xpath"
@@ -35,4 +57,23 @@ chrome_driver_path = "C:\\Development\\chromedriver-win64\\chromedriver.exe"
 service = ChromeService(chrome_driver_path)
 
 driver = webdriver.Chrome(service=service, options=option)
-driver.get("https://www.instagram.com/")
+for n in range(len(all_links)):
+    # Substitute your own Google Form URL here 👇
+    driver.get("https://docs.google.com/forms/d/e/1FAIpQLSfeFK6dtLV1kbmtHdDOXMtWWVtrE3utFhr7iiOWcX-ZkNgabw/viewform?usp=sf_link")
+
+    time.sleep(6)
+    address = driver.find_element(XPATH,
+                                  '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(XPATH,
+                                '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(XPATH,
+                               '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(XPATH,
+                                        '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
+# driver.get("https://docs.google.com/forms/d/e/1FAIpQLSfeFK6dtLV1kbmtHdDOXMtWWVtrE3utFhr7iiOWcX-ZkNgabw/viewform?usp=sf_link")
+    driver.quit()
